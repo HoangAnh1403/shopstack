e30c78b1bf98c1e85192a8b343febd96ca4f5018
Jane Hoang Anh <van.a@shopstack.asia>
2019-03-01 10:11:15 +0700

    Updates

---
+++ precache-manifest.e7976500f2eb6ef5e73c7b43785e465d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5dfc78242b7c1f8845cd",
+    "url": "/shopstack/static/js/runtime~main.5dfc7824.js"
+  },
+  {
+    "revision": "1300fcd831c54f03ff43",
+    "url": "/shopstack/static/js/main.1300fcd8.chunk.js"
+  },
+  {
+    "revision": "002c8bb4f02cada80a5b",
+    "url": "/shopstack/static/js/2.002c8bb4.chunk.js"
+  },
+  {
+    "revision": "1300fcd831c54f03ff43",
+    "url": "/shopstack/static/css/main.a54fa86a.chunk.css"
+  },
+  {
+    "revision": "9ccdeef68a178b3b9a941435c827737d",
+    "url": "/shopstack/index.html"
+  }
+];
